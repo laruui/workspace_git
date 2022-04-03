270801055d5c03ebd8e5e06a7a74106c7eeba081
laruui <laruuo@guigu.com>
2022-04-03 11:25:06 +0800

    first commit file

---
+++ src/main/java/com/google/git/TestPerson.java
@@ -0,0 +1,12 @@
+package com.google.git;
+
+/**
+ * @author CH
+ * @date 2022/4/3 - 11:24
+ */
+public class TestPerson {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+    }
+}
